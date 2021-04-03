81f126afbb05e07c23af7dd1919d1ac0d26afddf
Kyn21kx <nefesmeralda@gmail.com>
2021-04-02 20:55:47 -0600

    Initial commit

---
+++ Assets/Scripts/ImageHandler.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Manages an image, creating a 3x5 matrix containing each a part of an image
+/// </summary>
+public class ImageHandler : MonoBehaviour {
+
+	public struct Tile {
+
+		public GameObject gameObject;
+		public Texture2D texture;
+		public bool IsLast { get { return !gameObject.activeSelf; } }
+
+		public static bool operator ==(Tile a, Tile b) {
+			return a.gameObject == b.gameObject && a.texture == b.texture;
+        }
+
+		public static bool operator !=(Tile a, Tile b) {
+			return !(a == b);
+		}
+
+		public void SetTexture(Texture2D texture) {
+			var renderer = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+			renderer.material.SetTexture("_MainTex", texture);
+			this.texture = texture;
+		}
+
+	}
+
+	#region Variables
+	[SerializeField]
+	private Texture2D originalImage;
+	[SerializeField]
+	private GameObject baseTile;
+	[SerializeField]
+	private Vector3 startingPos;
+	[SerializeField]
+	private float scaleDownFactor;
+	[SerializeField]
+	private float transformDownFactor;
+	public int horizontal;
+	public int vertical;
+	/// <summary>
+	/// Once a movement is completed, we can check for the tile matrix + lastTile to be equal to the original matrix
+	/// </summary>
+	public Tile lastTile;
+	private Tile[,] originalTM;
+	public Tile[,] TileMatrix { get; private set; }
+	#endregion
+
+	private void Awake() {
+		SpawnParts();
+	}
+
+	public void SpawnParts() {
+		TileMatrix = GetTiles();
+		Tile[,] shuffled = (Tile[,])TileMatrix.Clone();
+		Utils.Randomize(shuffled);
+		for (int i = 0; i < horizontal; i++) {
+			for (int j = 0; j < vertical; j++) {
+				TileMatrix[i, j].SetTexture(shuffled[i, j].texture);
+			}
+		}
+		SetGlobalTiles();
+	}
+
+	private void SetGlobalTiles() {
+		int indexI = TileMatrix.GetLength(0) - 1, indexJ = TileMatrix.GetLength(1) - 1;
+		lastTile = TileMatrix[indexI, indexJ];
+		TileMatrix[indexI, 0].gameObject.SetActive(false);
+		originalTM = (Tile[,])TileMatrix.Clone();
+	}
+
+	private Tile[,] GetTiles() {
+		float nWidth = originalImage.width / (float)horizontal;
+		float nHeight = originalImage.height / (float)vertical;
+
+		Tile[,] tiles = new Tile[horizontal, vertical];
+		
+		int roundedHeight = Mathf.FloorToInt(nHeight);
+		int roundedWidth = Mathf.FloorToInt(nHeight);
+
+		//Starting pos + nWidth gives you the next positions
+		for (int i = 0; i < horizontal; i++) {
+			for (int j = 0; j < vertical; j++) {
+				Texture2D nTexture = new Texture2D(roundedWidth, roundedHeight, originalImage.format, false);
+				(int x, int y) pos;
+				pos.x = roundedWidth * i;
+				pos.y = roundedHeight * j;
+				Color[] pixelChunk = originalImage.GetPixels(pos.x, pos.y, roundedWidth, roundedHeight);
+				nTexture.SetPixels(pixelChunk);
+				byte[] bytes = nTexture.EncodeToPNG();
+				//Create a new game object with the proper sprite attached
+				var spawned = Instantiate(baseTile, this.transform);
+				spawned.transform.GetChild(0).name = $"Tile {i},{j}";
+				Vector3 position = new Vector3(startingPos.x + (i * nWidth), startingPos.y + (j * nHeight), 0f);
+				//Spawn them in a random position within the
+				spawned.transform.position = position / transformDownFactor;
+				spawned.transform.localScale = new Vector3(nWidth - 1, nHeight - 1, spawned.transform.localScale.z) / scaleDownFactor;
+				//Think of moving these to hard drive to avoid using too much memory
+				nTexture.LoadImage(bytes);
+				Tile t = new Tile();
+				t.gameObject = spawned;
+				t.texture = nTexture;
+				tiles[i, j] = t;
+			}
+		}
+		return tiles;
+	}
+
+	public bool IsOriginalArrangement() {
+        //Cycle through the array and compare their textures
+        for (int i = 0; i < TileMatrix.GetLength(0); i++) {
+            for (int j = 0; j < TileMatrix.GetLength(1); j++) {
+				if (TileMatrix[i, j].texture != originalTM[i, j].texture)
+					return false;
+            }
+        }
+		return true;
+    }
+}
---
+++ Assets/Scripts/TileShuffler.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class TileShuffler : MonoBehaviour {
+
+	public enum Directions {
+		UP = 2,
+		DOWN = -2,
+		LEFT = -1,
+		RIGHT = 1,
+		NONE = 3
+    }
+
+	#region Variables
+	private const int TILE_MASK = 1 << 8;
+	private Camera cam;
+	[SerializeField]
+	private ImageHandler handlerRef;
+	private Transform selectedTileT;
+	private bool selected;
+	private Directions direction;
+	#endregion
+
+	//Do mouse picking (Lifting the tile slighlty, and moving it to 
+	//a normalized vector multiplied by a fixed scalar factor
+
+	private void Start() {
+		cam = Camera.main;
+		direction = Directions.NONE;
+	}
+
+	private void ManageInput() {
+		if (Input.GetMouseButtonDown(0) && selectedTileT == null) {
+			SelectTile();
+		}
+		else if (Input.GetMouseButtonUp(0) && selectedTileT != null) {
+			Debug.Log("Tile " + selectedTileT.name + " has been deselected!");
+			selected = false;
+			MoveTile();
+			selectedTileT = null;
+		}
+	}
+
+	private void Update() {
+		ManageInput();
+		GrabTile();
+	}
+
+	private void MoveTile() {
+		Debug.Log(direction);
+		//Get the tile's indeces
+		StringBuilder sb = new StringBuilder(selectedTileT.name.Replace("Tile ", ""));
+		string[] strIndeces = sb.ToString().Split(',');
+		(int i, int j) indeces = (System.Convert.ToInt32(strIndeces[0]), System.Convert.ToInt32(strIndeces[1]));
+		(int i, int j) prevIndeces = indeces;
+		Debug.Log(indeces);
+		//Switch
+        switch (direction) {
+            case Directions.UP:
+            case Directions.DOWN:
+				int value = Mathf.Abs((int)direction);
+				indeces.j += (int)Mathf.Sign((int) direction) * (value - 1);
+                break;
+            case Directions.LEFT:
+            case Directions.RIGHT:
+				indeces.i += (int)direction;
+                break;
+            case Directions.NONE:
+                return;
+        }
+		//Check for bounds
+		if (!IsValidMove(indeces)) {
+			Debug.Log("Invalid move!");
+        }
+		else {
+			//Set active the empty tile object, and
+			//Swap their textures
+			ref ImageHandler.Tile prevIndexed = ref handlerRef.TileMatrix[prevIndeces.i, prevIndeces.j];
+			ref ImageHandler.Tile futureIndexed = ref handlerRef.TileMatrix[indeces.i, indeces.j];
+			
+			Texture2D tempTex = futureIndexed.texture;
+			futureIndexed.SetTexture(prevIndexed.texture);
+			prevIndexed.SetTexture(tempTex);
+			futureIndexed.gameObject.SetActive(true);
+			prevIndexed.gameObject.SetActive(false);
+			
+			if(handlerRef.IsOriginalArrangement()) {
+				//You've won
+				Debug.Log("You've won!");
+            }
+		}
+	}
+
+	private bool IsValidMove((int i, int j) indeces) {
+		int xLength = handlerRef.TileMatrix.GetLength(0);
+		int yLength = handlerRef.TileMatrix.GetLength(1);
+		return !(indeces.i < 0 || indeces.i >= xLength || indeces.j >= yLength || indeces.j < 0)
+			&& handlerRef.TileMatrix[indeces.i, indeces.j].IsLast;
+
+	}
+
+	private unsafe void GrabTile() {
+		if (selected) {
+			//Move around and calculate dir
+			//Get direction with mouse position
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = 10f;
+			Vector2 dir = (cam.ScreenToWorldPoint(mousePos) - selectedTileT.position);
+			dir.Normalize();
+			Debug.DrawRay(selectedTileT.position, dir);
+			//Now, get the int representation of the direction you want the tile to move
+			float* p = Utils.GetMaxAbsPointer(&dir.x, &dir.y);
+			//Give me 2 * sign if X, if Y 1 * sign
+			direction = p == &dir.x ? (Directions) (1 * Mathf.Sign(*p)) : (Directions) (2 * Mathf.Sign(*p));
+		}
+	}
+
+	private void SelectTile() {
+		//Clone the tile, and hide the other one to simulate movement
+		//Setup layermask to make sure that you cannot grab between collisions
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 20f, TILE_MASK)) {
+			selectedTileT = hit.transform;
+			selected = true;
+			Debug.Log("Selected: " + selectedTileT.name);
+		}
+	}
+
+}
