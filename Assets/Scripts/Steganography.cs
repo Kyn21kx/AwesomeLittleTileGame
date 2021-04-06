372ce7edde07b663e60951a5f67ea7493d70a57e
Kyn21kx <nefesmeralda@gmail.com>
2021-04-06 14:02:56 -0500

    Encoded succesfully

--- Assets/Scripts/Steganography.cs
+++ Assets/Scripts/Steganography.cs
@@ -8,34 +8,36 @@ public class Steganography : MonoBehaviour {
 	public Texture2D image;
 
 	private void Start() {
-		//Encrypt(image, "Test");
-		byte[] arr = SplitByte(88);
-		for (int i = 0; i < arr.Length; i++) {
-			Debug.Log(arr[i]);
-		}
+		Encrypt(image, "Test");
 	}
 
 	public void Encrypt(Texture2D image, string message) {
-		int bitIndex = 0;
-		Texture2D encryptedImage = new Texture2D(image.width, image.height, image.format, false);
+		int byteIndex = 0, bitIndex = 0;
+		//Texture2D encryptedImage = new Texture2D(image.width, image.height, image.format, false);
 		for (int y = 0; y < image.height; y++) {
 			for (int x = 0; x < image.width; x++) {
-				if (bitIndex >= message.Length) break;
-
+				if (byteIndex >= message.Length) break;
+				//Split the byte into its bit components
+				byte[] currentByte = SplitByte((byte)message[byteIndex]);
 				//Apply a bitwise or to each pixel
 				Color originalPixel = image.GetPixel(x, y);
 				//Create a new pixel modifying the blue channel
 				byte originalBlue = (byte)(originalPixel.b * 255);
-				byte n_blue = (byte)(originalBlue | (byte)message[bitIndex]);
-				Debug.Log($"Original blue: {(byte)originalPixel.b}, message byte: {(byte)message[bitIndex]}, encrypted blue: {n_blue}");
+				byte n_blue = (byte)(originalBlue | currentByte[bitIndex]);
+				Debug.Log($"Original blue: {originalBlue}, message byte: {currentByte[bitIndex]}, encrypted blue: {n_blue}");
 				Color encryptedColor = new Color(originalPixel.r, originalPixel.g, n_blue / 255f, originalPixel.a);
-				encryptedImage.SetPixel(x, y, encryptedColor);
+				image.SetPixel(x, y, encryptedColor);
 				bitIndex++;
+				if (bitIndex >= 8) {
+					bitIndex = 0;
+					byteIndex++;
+				}
 			}
 		}
 
-		byte[] data = encryptedImage.EncodeToJPG();
-		File.WriteAllBytes($"./Assets/Sprites/Encoded_{image.name}.jpg", data);
+		image.Apply();
+		byte[] data = image.EncodeToPNG();
+		File.WriteAllBytes($"./Assets/Sprites/Encoded_{image.name}.png", data);
 	}
 
 	private byte[] SplitByte(byte b) {
@@ -50,6 +52,7 @@ public class Steganography : MonoBehaviour {
 	}
 
 	public string Decrypt(Texture2D image) {
+
 		return null;
 	}
 
